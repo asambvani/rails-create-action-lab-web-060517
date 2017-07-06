f57064578ee3e5369f0efdad9022aaa32844804f
asambvani <alexsambvani@gmail.com>
2017-07-06 14:29:43 -0400

    Done.

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,6 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-
       t.timestamps null: false
     end
   end
